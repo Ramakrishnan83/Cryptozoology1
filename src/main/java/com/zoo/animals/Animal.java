93b96ea609163417b1652f8b8f5bbe2df3a60e84
Joe Durazo <jose.durazojr@gmail.com>
2021-01-09 15:03:22 -0600

    crated first get animals mapping

---
+++ src/main/java/com/zoo/animals/Animal.java
@@ -0,0 +1,14 @@
+package com.zoo.animals;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Animal {
+
+    @GetMapping("api/animals")
+    public void getAllAnimals() {
+
+    }
+
+}
